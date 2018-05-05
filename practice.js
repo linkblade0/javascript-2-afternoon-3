d6b229878cb41c792c3635cd09faeb9ceab9d679
unknown <josephlago90@gmail.com>
2018-05-05 12:16:04 -0600

    Completed practice.js

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first (arr, cb) {
+  cb(arr[0]);
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+const last = (arr, cb) => {
+  cb(arr[arr.length-1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,7 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
-
+const multiply = (num1, num2, cb) => {
+  cb(num1*num2)
+}
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,6 +93,10 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+const contains = (arr, name, cb) => {
+  return arr.includes(name)?cb(true):cb(false);
+}
+
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,7 +118,10 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
-
+const uniq = (arr, cb) => {
+  let newUniq = [...new Set (arr)]
+  cb(newUniq)
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -123,7 +138,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
-
+const each = (arr, cb) => {
+  arr.forEach(item => {
+    cb(item, arr.indexOf(item))
+  })
+}
 // Do not edit the code below.
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
@@ -140,7 +159,13 @@ each(names, function(item, indice){
 */
 
 // Code here
-
+const getUserById = (arr, id, cb) => {
+  for(var i=0;i<arr.length;i++){
+    if(arr[i].id === id) {
+      cb(arr[i])
+    }
+  }
+}
 // Do not edit the code below.
 var users = [
   {
